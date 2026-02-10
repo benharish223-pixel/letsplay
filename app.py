dba9693d3a14d6f69ea6f0d8397f9fc783bbdd00
benharish223-pixel <benharish223@gmail.com>
2026-02-10 22:40:58 +0530

    Create app.py file with Flask HTTP server

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-from flask import Flask\n\napp = Flask(__name__)\n\n@app.route('/')\ndef home():\n    return 'Hello, Flask!'\n\nif __name__ == '__main__':\n    app.run(host='0.0.0.0', port=5000)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome to the Flask HTTP Server!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
